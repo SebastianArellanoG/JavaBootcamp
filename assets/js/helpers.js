bde1efc13122998c0bb3e3893dc1caf55a8874ab
Sebastian Arellano <sebastianarellano@Mac-Studio-de-Sebastian.local>
2024-03-05 04:54:41 -0300

    stylos finales

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -8,10 +8,10 @@ Object.keys(cupones).forEach((cuponId) => {
         <div class="card" style="width: 19rem; margin: 20px auto;">
   <img src="${cupon.imglink}" class="card-img-top" alt="${cupon.title}" />
   <div class="card-body" >
-    <h5 class="card-title">${cupon.title}</h5>
-    <p class="card-text"><strong>Nombre:</strong> ${cupon.name}</p>
-    <p class="card-text" style="border-bottom: solid 1px  #C8C6C4 ;"> ${cupon.direcction}</p>
-    <p class="card-text text-end"><span class= "mx-2 small "style="color:#C8C6C4"> ${cupon.price}  </span><span class="text-success lead ">${cupon.salePrice}</span></p>
+    <h5 class="card-title text-ligth">${cupon.title}</h5>
+    <span class="card-text text-gray text-ligth">${cupon.name}</span>
+    <p class="card-text text-gray text-ligth" style="border-bottom: solid 1px  #C8C6C4 ;"><img src="/assets/svg/map-pin-alt-svgrepo-com.svg"> ${cupon.direcction}</p>
+    <p class="card-text text-end"><span class= "mx-2 small text-gray text-ligth"> ${cupon.price}  </span><span class="text-green lead ">${cupon.salePrice}</span></p>
 
   </div>
 </div>
